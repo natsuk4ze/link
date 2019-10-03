b7410616da3798a2c099e6a8ef714e6fecae5408
YutaTachibana0310 <yuta.tachibana0310@gmail.com>
2019-10-03 15:59:08 +0900

    文字列に関するフレームワークを追加

---
+++ project/Framework/String/String.cpp
@@ -0,0 +1,35 @@
+//=====================================
+//
+//Template.cpp
+//機能:文字列操作
+//Author:GP12B332 21 立花雄太
+//
+//=====================================
+#include "String.h"
+
+/**************************************
+文字列分割処理
+***************************************/
+void String::Split(std::vector<std::string>& out, std::string & str, const char delim)
+{
+	unsigned first = 0;						//始点
+	unsigned last = str.find_first_of(delim);	//終点（カンマの位置）
+
+	while (first < str.size())
+	{
+		//カンマの位置までの文字列を切り出す
+		std::string subStr(str, first, last - first);
+
+		out.push_back(subStr);
+
+		//次にカンマが現れるとこまで始点と終点を更新
+		first = last + 1;
+		last = str.find_first_of(delim, first);
+
+		if (last == std::string::npos)
+		{
+			last = str.size();
+		}
+
+	}
+}
---
+++ project/Framework/String/String.h
@@ -0,0 +1,23 @@
+//=====================================
+//
+//String.h
+//‹@”\:•¶š—ñ‘€ì
+//Author:GP12B332 21 —§‰Ô—Y‘¾
+//
+//=====================================
+#ifndef _STRING_H_
+#define _STRING_H_
+
+#include "../../main.h"
+#include <vector>
+
+namespace String
+{
+	/**************************************
+	•¶š—ñ•ªŠ„ŠÖ”
+	***************************************/
+	void Split(std::vector<std::string>& out, std::string& str, const char delim);
+
+}
+
+#endif
