31d647606322955e3bc7ca7425547af56c99c762
YutaTachibana0310 <yuta.tachibana0310@gmail.com>
2019-10-10 16:55:10 +0900

    ActorControllerを作成

---
+++ project/source/Field/PlaceActorController.cpp
@@ -0,0 +1,234 @@
+//=====================================
+//
+//PlaceActorController.cpp
+//機能:プレイスアクターコントローラ
+//Author:GP12B332 21 立花雄太
+//
+//=====================================
+#include "PlaceActorController.h"
+#include "Place\FieldPlaceModel.h"
+#include "Route\RouteModel.h"
+#include "FieldConfig.h"
+
+#include "../FieldObject/CityActor.h"
+#include "../FieldObject/CrossJunctionActor.h"
+#include "../FieldObject/CurveRoadActor.h"
+#include "../FieldObject/MountainActor.h"
+#include "../FieldObject/RiverActor.h"
+#include "../FieldObject/StraightRoadActor.h"
+#include "../FieldObject/TJunctionActor.h"
+
+namespace Field::Actor
+{
+	/**************************************
+	using宣言
+	***************************************/
+	using Model::PlaceType;
+	using Model::Adjacency;
+
+	/**************************************
+	コンストラクタ
+	***************************************/
+	PlaceActorController::PlaceActorController()
+	{
+		actorMap.resize(ActorPattern::Max, NULL);
+
+		actorMap[ActorPattern::City] = &cityContainer;
+		actorMap[ActorPattern::CrossJunction] = &crossJunctionContainer;
+		actorMap[ActorPattern::TJunction] = &tJunctionContainer;
+		actorMap[ActorPattern::Curve] = &curveContainer;
+		actorMap[ActorPattern::Mountain] = &mountainContainer;
+		actorMap[ActorPattern::River] = &riverContainer;
+		actorMap[ActorPattern::StarightRoad] = &straightContainer;
+	}
+
+	/**************************************
+	デストラクタ
+	***************************************/
+	PlaceActorController::~PlaceActorController()
+	{
+		for (auto&& map : actorMap)
+		{
+			map->clear();
+		}
+		actorMap.clear();
+	}
+
+	/**************************************
+	更新処理
+	***************************************/
+	void PlaceActorController::Update()
+	{
+		for (auto&& map : actorMap)
+		{
+			for (auto&& pair : *map)
+			{
+				pair.second->Update();
+			}
+		}
+	}
+
+	/**************************************
+	描画処理
+	***************************************/
+	void PlaceActorController::Draw()
+	{
+		//NOTE:インスタンシングで描画するために結構いじるかも
+		for (auto&& map : actorMap)
+		{
+			for (auto&& pair : *map)
+			{
+				pair.second->Draw();
+			}
+		}
+	}
+
+	/**************************************
+	アクターセット処理
+	***************************************/
+	void PlaceActorController::SetActor(const Model::PlaceModel * place)
+	{
+		const Model::PlaceType Type = place->GetType();
+
+		switch (Type)
+		{
+		case PlaceType::Bridge:
+			SetBridge(place);
+			break;
+
+		case PlaceType::Junction:
+			SetJunction(place);
+			break;
+
+		case PlaceType::Mountain:
+			SetMountain(place);
+			break;
+
+		case PlaceType::River:
+			SetRiver(place);
+			break;
+
+		case PlaceType::Road:
+			SetRoad(place);
+
+		case PlaceType::Town:
+			SetTown(place);
+			break;
+		}
+	}
+
+	/**************************************
+	アクターセット処理
+	***************************************/
+	void PlaceActorController::SetActor(const RouteModelPtr routeModel)
+	{
+		auto places = routeModel->GetAllPlaces();
+		
+		ChangeActor(places.front());
+		ChangeActor(places.back());
+
+		unsigned PlaceMax = places.size() - 1;
+		for(unsigned i = 0; i < PlaceMax; i++)
+		{ 
+			SetActor(places[i]);
+		}
+	}
+
+	/**************************************
+	アクター切り替え処理
+	***************************************/
+	void PlaceActorController::ChangeActor(const Model::PlaceModel * place)
+	{
+	}
+
+	/**************************************
+	ロードセット処理
+	***************************************/
+	void PlaceActorController::SetRoad(const Model::PlaceModel * place)
+	{
+		FieldPosition position = place->GetPosition();
+		D3DXVECTOR3 actorPos{ position.x * 10.0f, 0.0f, position.z * 10.0f };
+
+		//連結情報からどのタイプの道かを判定
+		std::vector<Adjacency> AdjacencyType = place->GetConnectingAdjacency();
+		StraightType straightType = IsStraight(AdjacencyType);
+
+		//直線タイプの場合
+		if (straightType != StraightType::NotStraight)
+		{
+			//アクター生成
+			PlaceActor* actor = new StraightRoadActor(actorPos, Model::FieldLevel::City);
+
+			//左右に繋がるタイプなら回転させる
+			if (straightType == StraightType::RightAndLeft)
+				actor->Rotate(90.0f);
+
+			AddContainer(ActorPattern::StarightRoad, place->ID(), actor);
+		}
+		//カーブの場合
+		else
+		{
+			//アクター生成
+			PlaceActor* actor = new CurveRoadActor(actorPos, Model::FieldLevel::City);
+			
+			//回転角度を決定して回転
+			float rotAngle = 0.0f;
+			
+			CurveType curveType = IsCurve(AdjacencyType);
+			if (curveType == CurveType::BackAndLeft)
+				rotAngle = -90.0f;
+			else if (curveType == CurveType::BackAndRight)
+				rotAngle = 180.0f;
+			else if (curveType == CurveType::ForwardAndRight)
+				rotAngle = 90.0f;
+
+			actor->Rotate(rotAngle);
+
+			AddContainer(ActorPattern::Curve, place->ID(), actor);
+		}
+	}
+
+	/**************************************
+	タウンセット処理
+	***************************************/
+	void PlaceActorController::SetTown(const Model::PlaceModel * place)
+	{
+	}
+
+	/**************************************
+	川セット処理
+	***************************************/
+	void PlaceActorController::SetRiver(const Model::PlaceModel * place)
+	{
+	}
+
+	/**************************************
+	ブリッジセット処理
+	***************************************/
+	void PlaceActorController::SetBridge(const Model::PlaceModel * place)
+	{
+	}
+
+	/**************************************
+	交差点セット処理
+	***************************************/
+	void PlaceActorController::SetJunction(const Model::PlaceModel * place)
+	{
+	}
+
+	/**************************************
+	山セット処理
+	***************************************/
+	void PlaceActorController::SetMountain(const Model::PlaceModel * place)
+	{
+	}
+
+	/**************************************
+	コンテナ追加処理
+	***************************************/
+	void PlaceActorController::AddContainer(ActorPattern pattern, unsigned key, PlaceActor * actor)
+	{
+		actorMap[pattern]->emplace(key, actor);
+	}
+
+}
---
+++ project/source/Field/PlaceActorController.h
@@ -0,0 +1,87 @@
+//=====================================
+//
+//PlaceActorController.h
+//機能:プレイスアクターコントローラ
+//Author:GP12B332 21 立花雄太
+//
+//=====================================
+#ifndef _PLACEACTORCONTROLLER_H_
+#define _PLACEACTORCONTROLLER_H_
+
+#include "../../main.h"
+#include "Place\PlaceConfig.h"
+
+#include <vector>
+#include <unordered_map>
+
+/**************************************
+前方宣言
+***************************************/
+class PlaceActor;
+namespace Field::Model
+{
+	class PlaceModel;
+	class RouteModel;
+}
+
+namespace Field::Actor
+{
+	/**************************************
+	前方宣言
+	***************************************/
+	using RouteModelPtr = std::shared_ptr<Field::Model::RouteModel>;
+	using ActorContainer = std::unordered_map<unsigned, std::unique_ptr<PlaceActor>>;
+	using ActorMap = std::vector<ActorContainer*>;
+
+	/**************************************
+	クラス定義
+	***************************************/
+	class PlaceActorController
+	{
+	public:
+		PlaceActorController();
+		~PlaceActorController();
+
+		void Update();
+		void Draw();
+
+		void SetActor(const Model::PlaceModel* place);
+		void SetActor(const RouteModelPtr routeModel);
+
+		void ChangeActor(const Model::PlaceModel* place);
+		
+	private:
+		//アクターパターン列挙子
+		enum ActorPattern
+		{
+			City,
+			CrossJunction,
+			TJunction,
+			Curve,
+			Mountain,
+			River,
+			StarightRoad,
+			Max
+		};
+
+		ActorMap actorMap;
+
+		ActorContainer cityContainer;
+		ActorContainer crossJunctionContainer;
+		ActorContainer tJunctionContainer;
+		ActorContainer curveContainer;
+		ActorContainer mountainContainer;
+		ActorContainer riverContainer;
+		ActorContainer straightContainer;
+
+		void SetRoad(const Model::PlaceModel* place);
+		void SetTown(const Model::PlaceModel* place);
+		void SetRiver(const Model::PlaceModel* place);
+		void SetBridge(const Model::PlaceModel* place);
+		void SetJunction(const Model::PlaceModel* place);
+		void SetMountain(const Model::PlaceModel* place);
+
+		void AddContainer(ActorPattern pattern, unsigned key, PlaceActor* actor);
+	};
+}
+#endif
