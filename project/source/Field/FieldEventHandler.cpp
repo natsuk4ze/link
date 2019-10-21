c4830280ee2e8d3a82660e706226695426953139
YutaTachibana0310 <yuta.tachibana0310@gmail.com>
2019-10-21 14:44:57 +0900

    FieldEventHandlerをstd::functionで実装

--- project/source/Field/FieldEventHandler.cpp
+++ project/source/Field/FieldEventHandler.cpp
@@ -11,137 +11,161 @@
 ***************************************/
 
 /**************************************
-
+コンストラクタ
 ***************************************/
-FieldEventHandler::FieldEventHandler()
+FieldEventHandler::FieldEventHandler() :
+	_DestroyTown(nullptr), _GetDestoryTarget(nullptr)
 {
+	functerInt.resize(FuncterID_int::FuncterIntMax, nullptr);
+	functerFloat.resize(FuncterID_float::FuncterFloatMax, nullptr);
+	functerBool.resize(FuncterID_bool::FuncterBoolMax, nullptr);
+	functerBoolReturn.resize(FuncterID_boolReturn::FuncterBoolReturnMax, nullptr);
+	functerVoid.resize(FuncterID_void::FuncterVoidMax, nullptr);
 }
 
 /**************************************
-
+デストラクタ
 ***************************************/
 FieldEventHandler::~FieldEventHandler()
 {
+	functerInt.clear();
+	functerFloat.clear();
+	functerBool.clear();
+	functerBoolReturn.clear();
 }
 
 /**************************************
-
+AI発展レベル調整
 ***************************************/
 void FieldEventHandler::AdjustLevelAI(float percent)
 {
+	functerFloat[FuncterID_float::AdjustAI](percent);
 }
 
 /**************************************
-
+AI発展レベル加算
 ***************************************/
 void FieldEventHandler::AddLevelAI(float num)
 {
+	functerFloat[FuncterID_float::AddAI](num);
 }
 
 /**************************************
-
+街全体のリンクレベル調整
 ***************************************/
 void FieldEventHandler::AdjustAllLinkLevel(int num)
 {
+	functerInt[FuncterID_int::AdjustLinkAll](num);
 }
 
 /**************************************
-
+街一つのリンクレベル調整
 ***************************************/
 void FieldEventHandler::AdjustLinkLevel(int num)
 {
+	functerInt[FuncterID_int::AdjustLink](num);
 }
 
 /**************************************
-
+ストック数加算
 ***************************************/
 void FieldEventHandler::AddStockNum(int num)
 {
+	functerInt[FuncterID_int::AddStock](num);
 }
 
 /**************************************
-
+発展ボーナス設定
 ***************************************/
 void FieldEventHandler::SetDevelopBonus(float percent)
 {
+	functerFloat[FuncterID_float::SetDevBonus](percent);
 }
 
 /**************************************
-
+新しい街生成
 ***************************************/
 void FieldEventHandler::CreateNewTown()
 {
+	functerVoid[FuncterID_void::Create]();
 }
 
 /**************************************
-
+街破壊処理
 ***************************************/
 void FieldEventHandler::DestroyTown(const Field::Model::PlaceModel * place)
 {
+	_DestroyTown(place);
 }
 
 /**************************************
-
+操作反転処理
 ***************************************/
 void FieldEventHandler::ReverseOperation(bool isReverse)
 {
+	functerBool[FuncterID_bool::ReverseOpe](isReverse);
 }
 
 /**************************************
-
+ストック封印処理
 ***************************************/
 void FieldEventHandler::SealUsingItem(bool isSeal)
 {
+	functerBool[FuncterID_bool::SealItem](isSeal);
 }
 
 /**************************************
-
+混雑度調整処理
 ***************************************/
-void FieldEventHandler::RaiseTrafficJam(float bias)
+void FieldEventHandler::AdjustTrafficJam(float bias)
 {
+	functerFloat[FuncterID_float::AdjustTraffic](bias);
 }
 
 /**************************************
-
+制限時間回復処理
 ***************************************/
-void FieldEventHandler::RecoveryRemainTime(int frame)
+void FieldEventHandler::AddRemainTime(int frame)
 {
+	functerInt[FuncterID_int::AddTime](frame);
 }
 
 /**************************************
-
+地球防衛軍が使えるか
 ***************************************/
 bool FieldEventHandler::TryUsingEDF()
 {
-	return false;
+	return functerBoolReturn[FuncterID_boolReturn::TryEDF]();
 }
 
 /**************************************
-
+保険を使えるか
 ***************************************/
 bool FieldEventHandler::TryUsingInsurance()
 {
-	return false;
+	return functerBoolReturn[FuncterID_boolReturn::TryInsurance]();
 }
 
 /**************************************
-
+ゲーム進行停止
 ***************************************/
 void FieldEventHandler::PauseGame()
 {
+	functerVoid[FuncterID_void::Pause]();
 }
 
 /**************************************
-
+ゲーム進行再開
 ***************************************/
 void FieldEventHandler::ResumeGame()
 {
+	functerVoid[FuncterID_void::Resume]();
 }
 
 /**************************************
-
+破壊対象取得処理
 ***************************************/
 const Field::Model::PlaceModel * FieldEventHandler::GetDestroyTarget()
 {
-	return nullptr;
+	return _GetDestoryTarget();
 }
--- project/source/Field/FieldEventHandler.h
+++ project/source/Field/FieldEventHandler.h
@@ -10,12 +10,20 @@
 
 #include "../../main.h"
 
+#include <functional>
+#include <vector>
+
 /**************************************
 前方宣言
 ***************************************/
 namespace Field::Model
 {
 	class PlaceModel;
+}
+
+namespace Field
+{
+	class FieldController;
 }
 
 /**************************************
@@ -27,26 +35,81 @@ public:
 	FieldEventHandler();
 	~FieldEventHandler();
 
-	void AdjustLevelAI(float percent);
-	void AddLevelAI(float num);
-	void AdjustAllLinkLevel(int num);
-	void AdjustLinkLevel(int num);
-	void AddStockNum(int num);
-	void SetDevelopBonus(float percent);
-	void CreateNewTown();
-	void DestroyTown(const Field::Model::PlaceModel* place);
-	void ReverseOperation(bool isReverse);
-	void SealUsingItem(bool isSeal);
-	void RaiseTrafficJam(float bias);
-	void RecoveryRemainTime(int frame);
-	bool TryUsingEDF();
-	bool TryUsingInsurance();
-	void PauseGame();
-	void ResumeGame();
-	const Field::Model::PlaceModel* GetDestroyTarget();
+	void AdjustLevelAI(float percent);							//AI発展レベルの調整処理
+	void AddLevelAI(float num);									//AI発展レベル加算処理
+	void AdjustAllLinkLevel(int num);							//街全体のリンクレベル調整処理
+	void AdjustLinkLevel(int num);								//街一つのリンクレベル調整処理
+	void AddStockNum(int num);									//アイテムストック加算処理
+	void SetDevelopBonus(float percent);						//発展度ボーナス設定処理
+	void CreateNewTown();										//新しい街作成処理
+	void DestroyTown(const Field::Model::PlaceModel* place);	//街破壊処理
+	void ReverseOperation(bool isReverse);						//操作反転処理
+	void SealUsingItem(bool isSeal);							//アイテムストック封印処理
+	void AdjustTrafficJam(float bias);							//混雑度調整処理
+	void AddRemainTime(int frame);								//制限時間回復処理
+	bool TryUsingEDF();											//地球防衛軍ストックの使用可否判定
+	bool TryUsingInsurance();									//保険ストックの使用可否判定
+	void PauseGame();											//ゲーム中断処理
+	void ResumeGame();											//ゲーム再開処理
+	const Field::Model::PlaceModel* GetDestroyTarget();			//破壊対象の街取得処理
 
 private:
+	//float型を引数にとるファンクタの通し番号
+	enum FuncterID_float
+	{
+		AdjustAI,
+		AddAI,
+		SetDevBonus,
+		AdjustTraffic,
+		FuncterFloatMax
+	};
 
+	//int型を引数に取るファンクタの通し番号
+	enum FuncterID_int
+	{
+		AdjustLinkAll,
+		AdjustLink,
+		AddStock,
+		AddTime,
+		FuncterIntMax
+	};
+
+	//bool型を引数に取るファンクタの通し番号
+	enum FuncterID_bool
+	{
+		ReverseOpe,
+		SealItem,
+		FuncterBoolMax
+	};
+
+	//引数を取らないboolを返すファンクタの通し番号
+	enum FuncterID_boolReturn
+	{
+		TryEDF,
+		TryInsurance,
+		FuncterBoolReturnMax
+	};
+
+	//引数を取らないファンクタの通し番号
+	enum FuncterID_void
+	{
+		Pause,
+		Resume,
+		Create,
+		FuncterVoidMax
+	};
+
+	std::vector<std::function<void(float)>> functerFloat;
+	std::vector<std::function<void(int)>> functerInt;
+	std::vector<std::function<void(bool)>> functerBool;
+	std::vector<std::function<bool(void)>> functerBoolReturn;
+	std::vector<std::function<void(void)>> functerVoid;
+
+	std::function<void(const Field::Model::PlaceModel*)> _DestroyTown;
+	std::function<const Field::Model::PlaceModel*(void)> _GetDestoryTarget;
+
+	friend class GameScene;
+	friend class Field::FieldController;
 };
 
 #endif
