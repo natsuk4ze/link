fbcd414d0bc7c26654be63a0315847afa00ebe74
YutaTachibana0310 <yuta.tachibana0310@gmail.com>
2019-11-07 17:16:47 +0900

    RiverActorの描画エフェクトクラスを作成

---
+++ project/source/Shader/RiverEffect.cpp
@@ -0,0 +1,73 @@
+//=====================================
+//
+//RiverEffect.cpp
+//機能:RiverActor描画エフェクト
+//Author:GP12B332 21 立花雄太
+//
+//=====================================
+#include "RiverEffect.h"
+#include "../../Framework/Resource/ResourceManager.h"
+
+namespace Field::Actor
+{
+	/**************************************
+	コンストラクタ
+	***************************************/
+	RiverEffect::RiverEffect()
+	{
+		//エフェクト
+		ResourceManager::Instance()->GetEffect("data/EFFECT/RiverRenderer.cfx", effect);
+
+		//ハンドル取得
+		hWorld = effect->GetParameterByName(0, "mtxWorld");
+		hView = effect->GetParameterByName(0, "mtxView");
+		hProjection = effect->GetParameterByName(0, "mtxProjection");
+
+		hMatDiffuse = effect->GetParameterByName(0, "materialDiffuse");
+		hMatAmbient = effect->GetParameterByName(0, "materialAmbient");
+		hMatSpecular = effect->GetParameterByName(0, "materialSpecular");
+
+		hLightDirection = effect->GetParameterByName(0, "lightDirection");
+		hLightDiffuse = effect->GetParameterByName(0, "lightDiffuse");
+		hLightAmbient = effect->GetParameterByName(0, "lightAmbient");
+		hLightSpecular = effect->GetParameterByName(0, "lightSpecular");
+
+		hTexUV = effect->GetParameterByName(0, "texUV");
+
+		effect->SetTechnique("tech");
+	}
+
+	/**************************************
+	デストラクタ
+	***************************************/
+	RiverEffect::~RiverEffect()
+	{
+		SAFE_RELEASE(effect);
+	}
+
+	/**************************************
+	ワールド情報設定処理
+	***************************************/
+	void RiverEffect::SetWorld(const Transform & transform)
+	{
+		D3DXMATRIX mtx = transform.GetMatrix();
+		effect->SetMatrix(hWorld, &mtx);
+	}
+
+	/**************************************
+	テクスチャUV設定処理
+	***************************************/
+	void RiverEffect::SetUV(const D3DXVECTOR2 & uv)
+	{
+		effect->SetFloatArray(hTexUV, (float*)&uv, 2);
+	}
+
+	/**************************************
+	カメラ情報コミット処理
+	***************************************/
+	void RiverEffect::CommitCameraParameter()
+	{
+		effect->SetMatrix(hView, &mtxView);
+		effect->SetMatrix(hProjection, &mtxProjection);
+	}
+}
