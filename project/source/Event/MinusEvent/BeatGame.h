c9ff908ee30a11af1a0c4e444890379289b3344a
Yu Oohama <bnban987@gmail.com>
2019-11-16 16:26:46 +0900

    BeatGame変更

--- project/source/Event/MinusEvent/BeatGame.h
+++ project/source/Event/MinusEvent/BeatGame.h
@@ -1,3 +1,7 @@
+//*****************************************************************************
+// 11/15(金)～おおはま改変
+//*****************************************************************************
+
 //=============================================================================
 //
 // 連打ゲームイベントクラス [BeatGame.h]
@@ -7,6 +11,7 @@
 #ifndef _BeatGame_H_
 #define _BeatGame_H_
 
+#include "../../Viewer/GameScene/BeatGameViewer/BeatTitleViewer.h"
 #include "../EventBase.h"
 #include <functional>
 #include <string>
@@ -14,10 +19,8 @@
 //*****************************************************************************
 // 前方宣言
 //*****************************************************************************
-class BaseViewerDrawer;
-class CountViewerDrawer;
-class TextViewer;
-class BeatGameViewerOrigin;
+class BeatGameViewer;
+class BeatTitleViewer;
 
 //*****************************************************************************
 // クラス定義
@@ -25,28 +28,49 @@ class BeatGameViewerOrigin;
 class BeatGame : public EventBase
 {
 private:
-	int RemainFrame;
-	int InputCount;
+	int countInput;
+	float countFrame;
 	bool TelopOver;
-	bool IsDrawingViewer;
-	bool IsSuccess;
-	BeatGameViewerOrigin *Viewer;
+	bool isSuccess;
+	bool isReady;
+	bool isFinished;
+	bool canSetReady;
+	bool canSetGo;
 	std::function<void(bool)> Callback;
 
 	void EventOver(void);
 
-	// テキスト
-	TextViewer *Text;
-	TextViewer *CountdownText;
-	// 小数点
-	BaseViewerDrawer *point;
-	// 少数部
-	CountViewerDrawer *fewNum;
-	// 整数部
-	CountViewerDrawer *intNum;
+	//成功したか判定処理
+	bool IsSuccess(void);
+
+	//失敗したか判定処理
+	bool IsFailed(void);
+
+	//レディーテキストセット処理
+	void SetReadyText(void);
+
+	//ゴーテキストセット処理
+	void SetGoText(void);
+
+	//連打ゲームビュアー
+	BeatGameViewer *beatGameViewer;
+
+	//ゲームタイトル取得
+	BeatTitleViewer::TitleID GetGameTitle(void);
 
 public:
-	BeatGame(std::function<void(bool)> CallBack = nullptr);
+
+	enum GameType
+	{
+		AILevelDecrease,
+		BanStockUse,
+		CityDestroyEvent,
+	};
+
+	//再生中のイベント
+	GameType playingEvent;
+
+	BeatGame(BeatGame::GameType type,std::function<void(bool)> CallBack = nullptr);
 	~BeatGame();
 	void Update(void) override;
 	void Draw(void) override;
