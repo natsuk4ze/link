9fa9953ba888fc5d5c8695a3781fd59b1499eba0
YutaTachibana0310 <yuta.tachibana0310@gmail.com>
2019-11-20 18:27:47 +0900

    スキンメッシュもResourceManagerを使用してテクスチャを読み込むように変更

--- project/Framework/Animation/MyAllocateHierarchy.cpp
+++ project/Framework/Animation/MyAllocateHierarchy.cpp
@@ -5,6 +5,7 @@
 //
 //=====================================
 #include "MyAllocateHierarchy.h"
+#include "../Resource/ResourceManager.h"
 
 /**************************************
 ƒ}ƒNƒ’è‹`
@@ -172,7 +173,7 @@ HRESULT MyAllocateHierarchy::CreateMeshContainer(THIS_
 				ZeroMemory(textureName, sizeof(textureName));
 				sprintf_s(textureName, "data/MODEL/%s", meshContainer->pMaterials[iMaterial].pTextureFilename);
 
-				if (FAILED(D3DXCreateTextureFromFile(pDevice, textureName, &meshContainer->textures[iMaterial])))
+				if (!ResourceManager::Instance()->GetTexture(textureName, meshContainer->textures[iMaterial]))
 				{
 					meshContainer->textures[iMaterial] = NULL;
 				}
