d0e7114f011b0a7b0a3ade9181aa8305fc72d1da
SomeTake <tp1002217g@gmail.com>
2019-11-20 17:49:43 +0900

    焦点座標を修正

--- project/source/Viewer/GameScene/GuideViewer/GuideCamera.cpp
+++ project/source/Viewer/GameScene/GuideViewer/GuideCamera.cpp
@@ -16,7 +16,7 @@ GuideCamera::GuideCamera()
 	const float ScreenWidth = 360.0f;
 	const float InitViewAspect = ScreenWidth / ScreenHeight;
 	const D3DXVECTOR3 InitPos = D3DXVECTOR3(0.0f, 64.0f, -40.0f);
-	const D3DXVECTOR3 InitTarget = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	const D3DXVECTOR3 InitTarget = D3DXVECTOR3(0.0f, 64.0f, 0.0f);
 	
 	transform.SetPosition(InitPos);
 	transform.LookAt(InitTarget);
