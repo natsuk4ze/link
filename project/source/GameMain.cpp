703365b0cb197f4b56214ebaa23f009cb073e232
YutaTachibana0310 <yuta.tachibana0310@gmail.com>
2019-11-29 11:46:54 +0900

    初期シーンをゲームシーンに修正

--- project/source/GameMain.cpp
+++ project/source/GameMain.cpp
@@ -34,7 +34,7 @@ GameMain::GameMain(HINSTANCE hInstance, HWND hWnd) :
 	PlayerPrefs::SaveNumber<int>(Utility::ToString(GameConfig::Key_FieldLevel), 0);
 
 	//‰ŠúƒV[ƒ“‚É‘JˆÚ
-	const int InitScene = GameConfig::SceneID::MophingTest;
+	const int InitScene = GameConfig::SceneID::Game;
 	sceneManager->ChangeScene(InitScene);
 }
 
