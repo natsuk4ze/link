fd84ae21f65b80dd11667a54d4bb2e941a63121e
YutaTachibana0310 <yuta.tachibana0310@gmail.com>
2019-11-29 13:49:23 +0900

    確認用の石モーフィングを追加

--- project/source/Scene/MophingTestScene.cpp
+++ project/source/Scene/MophingTestScene.cpp
@@ -19,113 +19,34 @@
 ***************************************/
 void MophingTestScene::Init()
 {
-	LPDIRECT3DDEVICE9 pDevice = GetDevice();
-	LPD3DXBUFFER tmpMaterial;
-	LPD3DXMESH mesh;
-
-	//Xファイル読み込み1
-	HRESULT res = D3DXLoadMeshFromX("data/MODEL/Mophing/ishi.x",
-		D3DXMESH_MANAGED,
-		pDevice,
-		NULL,
-		&tmpMaterial,
-		NULL,
-		&materialNum,
-		&mesh);
-
-	if (res != S_OK)
-		return;
-
-	//隣接情報を作成して最適化
-	std::vector<DWORD> adjList;
-	adjList.resize(3 * mesh->GetNumFaces());
-	mesh->GenerateAdjacency(1.0f / 512, &adjList[0]);
-	mesh->OptimizeInplace(D3DXMESHOPT_ATTRSORT, &adjList[0], 0, 0, 0);
-
-	//各バッファを取得
-	mesh->GetVertexBuffer(&vtx1);
-	mesh->GetIndexBuffer(&indexBuff);
-	fvf = mesh->GetFVF();
-
-	//属性テーブル取得
-	attributeTable.resize(materialNum);
-	mesh->GetAttributeTable(&attributeTable[0], NULL);
-
-	//マテリアルをD3DXMATERIALとして最適化
-	materials.resize(materialNum);
-	D3DXMATERIAL* matBuffer = (D3DXMATERIAL*)tmpMaterial->GetBufferPointer();
-	for (DWORD i = 0; i < materialNum; i++)
-	{
-		materials[i] = matBuffer[i].MatD3D;
-	}
-
-	//頂点宣言作成
-	D3DVERTEXELEMENT9 elem[65];
-	ZeroMemory(&elem[0], sizeof(D3DVERTEXELEMENT9) * 65);
-	mesh->GetDeclaration(elem);
-
-	for (int i = 0; i < 65; i++)
-	{
-		if (elem[i].Type != D3DDECLTYPE_UNUSED)
-			continue;
-
-		for (int j = i; j < i * 2; j++)
-		{
-			elem[j] = elem[j - i];
-			elem[j].Stream = 1;
-			elem[j].UsageIndex = 1;
-		}
-		elem[i * 2] = D3DDECL_END();
-		break;
-	}
-
-	pDevice->CreateVertexDeclaration(elem, &declare);
-
-	//Xファイル読み込み2
-	SAFE_RELEASE(mesh);
-	res = D3DXLoadMeshFromX("data/MODEL/Mophing/ishi2.x",
-		D3DXMESH_MANAGED,
-		pDevice,
-		NULL,
-		&tmpMaterial,
-		NULL,
-		&materialNum,
-		&mesh);
-
-	//隣接情報を作成して最適化
-	adjList.resize(3 * mesh->GetNumFaces());
-	mesh->GenerateAdjacency(1.0f / 512, &adjList[0]);
-	mesh->OptimizeInplace(D3DXMESHOPT_ATTRSORT, &adjList[0], 0, 0, 0);
-
-	//各バッファを取得
-	mesh->GetVertexBuffer(&vtx2);	
-	fvf2 = mesh->GetFVF();
-
-	//テクスチャ読み込み
-	D3DXCreateTextureFromFile(pDevice, "data/MODEL/Mophing/ishi.bmp", &texture);
-
-	SAFE_RELEASE(mesh);
-	SAFE_RELEASE(tmpMaterial);
-
-	effect = new MophingEffect();
-
 	sceneCamera = new Camera();
 	Camera::SetMainCamera(sceneCamera);
 
 	skybox = new Field::FieldSkyBox(Field::FieldLevel::World);
-
-	ResourceManager::Instance()->LoadMesh("ishi", "data/MODEL/Mophing/town.x");
-	ResourceManager::Instance()->LoadMesh("ishi2", "data/MODEL/Mophing/town_m.x");
-
 	morphContainer = new MorphingMeshContainer();
-	ResourceManager::Instance()->GetMesh("ishi", morphContainer);
+	stoneMesh = new MorphingMeshContainer();
+
+	ResourceManager::Instance()->LoadMesh("town", "data/MODEL/Mophing/town.x");
+	ResourceManager::Instance()->LoadMesh("town2", "data/MODEL/Mophing/town_m.x");
+	ResourceManager::Instance()->LoadMesh("ishi", "data/MODEL/Mophing/ishi.x");
+	ResourceManager::Instance()->LoadMesh("ishi2", "data/MODEL/Mophing/ishi2.x");
+
+	ResourceManager::Instance()->GetMesh("town", morphContainer);
 	morphContainer->RegisterVertex(0);
 
-	ResourceManager::Instance()->GetMesh("ishi2", morphContainer);
+	ResourceManager::Instance()->GetMesh("town2", morphContainer);
 	morphContainer->RegisterVertex(1);
+
+	ResourceManager::Instance()->GetMesh("ishi", stoneMesh);
+	stoneMesh->RegisterVertex(0);
+
+	ResourceManager::Instance()->GetMesh("ishi2", stoneMesh);
+	stoneMesh->RegisterVertex(1);
 
 	morphContainer->SetCurrent(0);
 	morphContainer->SetNext(1);
+
+	stoneMesh->SetNext(1);
 }
 
 /**************************************
@@ -148,6 +69,7 @@ void MophingTestScene::Uninit()
 	SAFE_DELETE(sceneCamera);
 
 	SAFE_DELETE(morphContainer);
+	SAFE_DELETE(stoneMesh);
 }
 
 /**************************************
@@ -169,19 +91,8 @@ void MophingTestScene::Draw()
 	
 	LPDIRECT3DDEVICE9 pDevice = GetDevice();
 
-	//pDevice->SetStreamSource(0, vtx1, 0, D3DXGetFVFVertexSize(fvf));
-	//pDevice->SetStreamSource(1, vtx2, 0, D3DXGetFVFVertexSize(fvf2));
-
-	//UINT freq0, freq1;
-	//pDevice->GetStreamSourceFreq(0, &freq0);
-	//pDevice->GetStreamSourceFreq(1, &freq1);
-
-	//pDevice->SetIndices(indexBuff);
-
-	//pDevice->SetVertexDeclaration(declare);
-
 	static Transform transform;
-	static D3DXVECTOR3 pos = Vector3::Forward * 1000.0f, scale = Vector3::One, rot = Vector3::Zero;
+	static D3DXVECTOR3 pos = Vector3::Forward * 200.0f, scale = Vector3::One, rot = Vector3::Zero;
 	static float t = 0.0f;
 
 	Debug::Begin("Mophing");
@@ -197,4 +108,9 @@ void MophingTestScene::Draw()
 
 	morphContainer->SetChange(t);
 	morphContainer->Draw(transform.GetMatrix());
+
+	transform.SetPosition({ 150.0f, 25.0f, 1000.0f });
+
+	stoneMesh->SetChange(t);
+	stoneMesh->Draw(transform.GetMatrix());
 }
