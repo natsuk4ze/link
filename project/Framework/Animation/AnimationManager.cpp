4044df6a898661e194f737c1757250cc59be388a
YutaTachibana0310 <yuta.tachibana0310@gmail.com>
2019-12-03 12:29:33 +0900

    スキンメッシュのアニメーションセットの更新を描画直前に呼ぶように変更

--- project/Framework/Animation/AnimationManager.cpp
+++ project/Framework/Animation/AnimationManager.cpp
@@ -45,9 +45,6 @@ void AnimationManager::Update()
 	{
 		ChangeAnim(transitionMap[currentAnimID], true);
 	}
-
-	//アニメーションの更新
-	container->Update(deltaTimeList[currentAnimID]);
 }
 
 /**************************************
@@ -55,6 +52,9 @@ void AnimationManager::Update()
 ***************************************/
 void AnimationManager::Draw(LPD3DXMATRIX mtxWorld)
 {
+	//アニメーションの更新
+	container->Update(deltaTimeList[currentAnimID]);
+
 	container->Draw(mtxWorld);
 }
 
