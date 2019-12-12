129e4a4b65234686d4b953eadbbe0f8d903c5237
YutaTachibana0310 <yuta.tachibana0310@gmail.com>
2019-12-12 21:23:10 +0900

    フィールドレベルアップ時のオブジェクト更新処理を追加

--- project/source/Scene/GameState/GameLevelUp.cpp
+++ project/source/Scene/GameState/GameLevelUp.cpp
@@ -8,6 +8,8 @@
 #include "GameLevelUp.h"
 #include "../../Field/FieldController.h"
 #include "../../Viewer/GameScene/Controller/GameViewer.h"
+#include "../../Field/FieldController.h"
+#include "../../Event/EventController.h"
 
 /**************************************
 入場処理
@@ -34,5 +36,8 @@ void GameScene::GameLevelUp::OnStart(GameScene & entity)
 ***************************************/
 GameScene::State GameScene::GameLevelUp::OnUpdate(GameScene & entity)
 {
+	//オブジェクト更新
+	entity.field->UpdateObject();
+	entity.eventController->UpdateViewer();
 	return State::LevelUp;
 }
