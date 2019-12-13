6b571c96543e7645f9b5753f7c96141f588788c3
YutaTachibana0310 <yuta.tachibana0310@gmail.com>
2019-12-13 10:39:11 +0900

    フィールド遷移時のViewer制御の修正

--- project/source/Scene/GameState/GameLevelUp.cpp
+++ project/source/Scene/GameState/GameLevelUp.cpp
@@ -16,19 +16,11 @@
 ***************************************/
 void GameScene::GameLevelUp::OnStart(GameScene & entity)
 {
-	//TODO:レベルアップ時の演出を再生する
 	// スコアの加算
 	entity.field->SetScore();
 
-	//本来は演出終了のコールバックで遷移させる
-
-	// グレードアップ
-	entity.gameViewer->SetGradeUp([&]()
-	{
-		//レベルアップ処理
-		entity.OnLevelUp();
-		entity.ChangeState(GameScene::State::TransitionOut);
-	});
+	//ステップを初期化
+	entity.step = Step::WaitLevelPopup;
 }
 
 /**************************************
@@ -36,6 +28,29 @@ void GameScene::GameLevelUp::OnStart(GameScene & entity)
 ***************************************/
 GameScene::State GameScene::GameLevelUp::OnUpdate(GameScene & entity)
 {
+	//ステップ実行
+	switch (entity.step)
+	{
+	case Step::WaitLevelPopup:
+		//リンクレベルポップアップを待つ必要がなくなったら演出開始
+		if (!entity.field->ShouldWaitPopup())
+		{
+			// グレードアップ
+			entity.gameViewer->SetGradeUp([&]()
+			{
+				//レベルアップ処理
+				entity.OnLevelUp();
+				entity.ChangeState(GameScene::State::TransitionOut);
+			});
+
+			entity.step = Step::TelopIn;
+		}
+
+	default:
+		break;
+	}
+
+
 	//オブジェクト更新
 	entity.field->UpdateObject();
 	entity.eventController->UpdateViewer();
